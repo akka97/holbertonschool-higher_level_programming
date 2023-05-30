f4cdf3932cac0f0c312c1f486e238eee19bf17eb
akka97 <akapellari@yahoo.com>
2023-05-30 01:29:49 -0700

    tasks

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[39:66] + ' with ' + str[:6])
-print('\n')
+str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax"
+concat_string = str[39:66] + str[107:113] + str[:6]
+print(concat_string)
+
