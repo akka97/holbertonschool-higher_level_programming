3cb744bb7604c87d114dd15d8a13788bddc2a92e
akka97 <akapellari@yahoo.com>
2023-05-30 01:02:20 -0700

    tasks

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = word[:2]
+word_first_3 = word[:3]
 print(f"First 3 letters: {word_first_3}")
 word_last_2 = word[-2:]
 print(f"Last 2 letters: {word_last_2}")
