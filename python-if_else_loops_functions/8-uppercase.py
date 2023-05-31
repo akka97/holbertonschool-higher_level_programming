c53d4f5a8bc052bb3f5caaa930cadc89ed168775
akka97 <akapellari@yahoo.com>
2023-05-31 04:45:59 -0700

    tasks

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@ def uppercase(str):
     for ch in str:
         val = ord(ch)
         if val >= 97 and val <= 122:
-            new += chr(val - 32)
+            new = new + chr(val - 32)
         else:
             new += ch
     print("{}".format(new))
