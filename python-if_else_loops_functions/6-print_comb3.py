71ce2e96753d2cd36d5fbcc13f0f60f42cb09f6b
akka97 <akapellari@yahoo.com>
2023-05-31 03:33:18 -0700

    tasks

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+for i in range(0, 9):
+    for j in range(1, 10):
+        if i + j == 17:
+            print("{}{}".format(i, j))
+        elif i != j and i < j:
+            print("{}{}".format(i, j), end=", ")
