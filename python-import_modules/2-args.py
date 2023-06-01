2d94bea428513cb41d61d49de2a9afa0f02a5a64
akka97 <akapellari@yahoo.com>
2023-06-01 06:36:59 -0700

    tasks

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    nr_arg = len(sys.argv) - 1
+if nr_arg == 0:
+    print(f"{nr_arg} arguments.")
+elif nr_arg == 1:
+    print(f"{nr_arg} argument:")
+else:
+    print(f"{nr_arg} arguments:")
+for index, arg in enumerate(sys.argv[1:], start=1):
+    print(f"{index}: {sys.argv[index]}")
