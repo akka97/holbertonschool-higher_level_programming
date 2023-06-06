e5bc16cd7b56e3102496b7f61e42f7859fc0b6b9
akka97 <akapellari@yahoo.com>
2023-06-06 05:07:15 -0700

    tasks

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    maxi = my_list[0]
+    for i in range(1, len(my_lsit)):
+        if my_list[i] > maxi:
+            maxi = my_list[i]
+    return maxi
