ffc092ed33dd929f7dc13adaadaed6ea6bc79a96
akka97 <akapellari@yahoo.com>
2023-06-06 04:56:23 -0700

    tasks

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    t1 = t1 += (0, 0)[:2 - len(t1)]
-    t2 = t2 += (0, 0)[:2 - len(t2)]
+    t1 += (0, 0)[:2 - len(t1)]
+    t2 += (0, 0)[:2 - len(t2)]
     result = (t1[0] + t2[0], t1[1] + t2[1])
     return result
