798a03a672cfec730e512707d48ae9613843a3c8
akka97 <akapellari@yahoo.com>
2023-06-09 01:42:01 -0700

    tasks

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,8 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        print()
-        return True
-    except (TypeError, ValueError):
+    except ValueError:
         return False
+    except TypeError:
+        return False
+    return True
