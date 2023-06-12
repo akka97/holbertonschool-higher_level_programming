803db6a856ecf7622f96e02f0e29e99dd8b04621
akka97 <akapellari@yahoo.com>
2023-06-12 06:34:09 -0700

    tasks

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,10 +3,11 @@
 
 
 class Square:
-    """init func"""
-    size: def __init__(self, size=0):
+    """init gfunc"""
+    def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
-        self-__size = size
+        else:
+        self.__size = size
