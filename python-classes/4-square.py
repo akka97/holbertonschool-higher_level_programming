4cdb34c82c8108cb2443065add921e273363308d
akka97 <akapellari@yahoo.com>
2023-06-13 03:23:48 -0700

    tasks

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -12,22 +12,22 @@ class Square:
         self.__size = size
 
 
-        """getter for size"""
-        @property
-        def size(self):
-            return self.__size
+    """getter for size"""
+    @property
+    def size(self):
+        return self.__size
 
 
-        """change size func"""
-        @size.setter
-        def size(self, value):
-            if type(value) is not int:
-                raise TypeError('size must be an integer')
-            if value < 0:
-                raise ValueError('size must be >= 0')
-            self.__size = value
+    """change size func"""
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
 
-        """find area of square"""
-        def area(self):
-            return self.__size ** 2
+    """find area of square"""
+    def area(self):
+        return self.__size ** 2
