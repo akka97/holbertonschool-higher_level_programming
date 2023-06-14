7c800bb0173ddc610110a6856034d7265ad974c0
akka97 <akapellari@yahoo.com>
2023-06-14 04:09:22 -0700

    tasks

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -45,8 +45,10 @@ class Rectangle:
             self.__height = value
 
     def area(self):
-        return self.area
+        return self.__width * self.__height
 
     def perimeter(self):
-        if type(width) or type(height) != 0:
-            return self.perimeter
+        if self.__width and self.height == 0:
+            return 0
+        else:
+            return 2 * self.__width + 2 * self.__height
