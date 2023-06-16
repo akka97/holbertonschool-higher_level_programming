6b7470ac5882e2d04997f8b5c499aacc204402cb
akka97 <akapellari@yahoo.com>
2023-06-16 03:05:46 -0700

    tasks

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+"""Text identation module"""
+
+
+def text_identation(text):
+    """Idents text when needed"""
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+    char = ['.', '?', ':']
+    i = 0
+    while i in range(len(text)):
+        print('{}'.format(text[i]), end="")
+        if text[i] in char:
+            print('\n')
+            while text[i + 1] == " ":
+                i += 1
+        i += 1
