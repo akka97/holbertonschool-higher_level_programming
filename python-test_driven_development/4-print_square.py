aa1e9bdb51fb52caab5c91d83dac2b32899a6ac4
akka97 <akapellari@yahoo.com>
2023-06-16 02:54:16 -0700

    tasks

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -12,5 +12,5 @@ def print_square(size):
         raise TypeError('size must be an integer')
     for i in range(size):
         for j in range(size):
-            print("#", end=")
+            print("#", end="")
         print()
