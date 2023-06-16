2a2733221b9ed5c20dd4e323da03ba5f8c6a346c
akka97 <akapellari@yahoo.com>
2023-06-16 04:14:30 -0700

    tasks

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Unittest for max_integer"""
+"""Unittest for max_integer([..])
+"""
 
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-class TestMaxInteger(unittest.Testcase):
+class TestMaxInteger(unittest.TestCase):
     def test_max(self):
         self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 5)
