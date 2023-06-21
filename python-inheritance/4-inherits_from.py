16a5ffc2c39c4ec1b1b5bd8c55559e6fcec596ef
akka97 <akapellari@yahoo.com>
2023-06-21 04:22:41 -0700

    tasks

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,7 +5,7 @@
 def inherits_from(obj, a_class):
     """Finds if object is in the same class"""
     if isinstance(obj, a_class):
-        if obj.__class__is a class:
+        if obj.__class__ is a_class:
             return False
         return True
     return False
