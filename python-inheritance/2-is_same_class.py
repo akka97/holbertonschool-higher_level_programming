95f60aa36dbdd99e250e359ff90088a2fbd71ab4
akka97 <akapellari@yahoo.com>
2023-06-21 04:07:42 -0700

    tasks

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""Module for objects and classes"""
+
+
 def is_same_class(obj, a_class):
-    return type(obj) is a_class
+    """Finds if an object is an instance of a class"""
+    if type(obj) is a_class:
+        return True
+    return False
