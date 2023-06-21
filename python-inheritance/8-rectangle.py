37eb8aa5b85ecf22aeb0eb6b98ca06b844af1f4b
akka97 <akapellari@yahoo.com>
2023-06-21 05:02:54 -0700

    tasks

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module for objects and classes"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Subclass of BaseGeometry"""
+    def __init__(self, width, height):
+        """initalisation of an object"""
+        BaseGeometry.integer_validator(self, 'width', width)
+        BaseGeometry.integer_validator(self, 'width', width)
+        self.__width = width
+        self.__height = height
