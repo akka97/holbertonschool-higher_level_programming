99e73ce0155d2cd0f88760f72c88836743ffc8d8
akka97 <akapellari@yahoo.com>
2023-06-21 03:35:03 -0700

    tasks

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python3
+"""list subclass"""
+
 class MyList(list):
+    """Prints a sorted list"""
     def print_sorted(self):
         sorted_list = sorted(self)
         print(sorted_list)
