cd9d200f859eafa98e99a4b9ee65df0b986a9811
akka97 <akapellari@yahoo.com>
2023-06-22 03:56:03 -0700

    tasks

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,7 +9,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
 if os.path.exists('ad_item.json') is not True:
-    with open('add_item.json', 'w', encoding='utf=8') as f:
+    with open('add_item.json', 'w', encoding='utf-8') as f:
         load = []
         save_to_json_file(load, 'add_item.json')
 load = load_from_json_file('add_item.json')
