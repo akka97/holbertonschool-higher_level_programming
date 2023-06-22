f8298435e185e0a0b40fefee1cdfa376ed4fbb93
akka97 <akapellari@yahoo.com>
2023-06-22 00:34:57 -0700

    tasks

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module for objects and classes"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Square(Rectangle):
+    """Subclass of Rectangle"""
+    def __init__(self, size):
+        """initalisation of square object"""
+        BaseGeometry.integer_validator(self, 'size', size)
+        Rectangle.__init__(self, self, size)
+        self.__size = size
+
+    def area(self):
+        """Returns area of square"""
+        return self.__size ** 2
