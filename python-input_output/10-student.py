ac568450ce58e89558e425efe0dcdae3ecf39e3e
akka97 <akapellari@yahoo.com>
2023-06-22 06:53:45 -0700

    tasks

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -9,7 +9,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(selfi, attrs=None):
+    def to_json(self, attrs=None):
         d = {}
         if attrs is None:
             for attr, value in self.__dict__.items():
