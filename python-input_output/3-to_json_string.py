587d023917bb9479e426689d3f0373e1e8e37fda
akka97 <akapellari@yahoo.com>
2023-06-22 01:36:24 -0700

    tasks

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Writing inside a file module"""
 
+
 import json
 
 
 def to_json_string(my_obj):
-    """writes in json string format"""
-    retrun json.dumps(my_obj)
+    """Write in sjon string format"""
+    return json.dumps(my_obj)
     return count
