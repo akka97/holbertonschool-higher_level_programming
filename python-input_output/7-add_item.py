60018ada435d1be1c368488514f75b54576253cb
akka97 <akapellari@yahoo.com>
2023-06-23 01:30:52 -0700

    tasks

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,7 +3,7 @@
 
 
 import os
-from sys import argv
+import sys 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
