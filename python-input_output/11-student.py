07d3253092c4fc107fb2b00585cf698fd043f33e
akka97 <akapellari@yahoo.com>
2023-06-23 01:08:36 -0700

    tasks

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,24 +1,8 @@
 #!/usr/bin/python3
-"""Module for square class"""
+"""Student class module"""
 
 
-Rectangle = __import__('9-rectangle').Rectangle
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
-class Square(Rectangle):
-    """Subclass square"""
-    def __init__(self, size):
-        """Initialisation of square object"""
-        BaseGeometry.integer_validator(self, 'size', size)
-        Rectangle.__init__(self, size, size)
-        self.__size = size
-
-    def area(self):
-        """Returns area of square"""
-        return self.__size ** 2
-
-    def __str__(self):
-        to_str = '[Square] '
-        to_str += str(self.__size) + '/' + str(self.__size)
-        return to_str
+class Student:
+    """Class student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name =first_name
