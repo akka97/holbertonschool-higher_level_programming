4cea2758fc2640b95580128fc8d3c75ecbe47c47
akka97 <akapellari@yahoo.com>
2023-06-23 01:11:16 -0700

    tasks

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -12,7 +12,7 @@ if os.path.exists('add_item.json') is not True:
     with open('add_item.json', 'w', encoding='utf-8') as f:
         load = []
         save_to_json_file(load, 'add_item.json')
-load = load_from_json_file('add_item.json')
+my_list = load_from_json_file('add_item.json')
 for i in range(len(argv)):
     if i != 0:
         load.append(argv[i])
