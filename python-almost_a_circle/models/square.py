54626f3b32cd83eff84b08a3b2bc558e6e8dde0c
akka97 <akapellari@yahoo.com>
2023-06-28 03:58:29 -0700

    tasks

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Inherted class'''
+'''Module for square class'''
 
 from models.rectangle import Rectangle
 
@@ -7,10 +7,10 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     '''Square class'''
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, x, y, size, size)
+        Rectangle.__init__(self, size, size, x, y, id)
 
     def __str__(self):
         tostr = "[Square] ({}) ".format(self.id)
         tostr += "{}/{} - ".format(self.x, self.y)
-        tostr += "{}".format(Self.size)
+        tostr += "{}".format(self.size)
         return tostr
