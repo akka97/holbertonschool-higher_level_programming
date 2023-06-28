4d2953ffa81ccf96d9e2f042c36c9732cd76c954
akka97 <akapellari@yahoo.com>
2023-06-28 04:58:07 -0700

    tasks

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,10 @@
 '''First Class'''
 
 
+import json
+import os
+
+
 class Base:
     '''Base Class'''
     __nb_objects = 0
@@ -12,3 +16,55 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''returns string representation of dictionary'''
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''Saves dictionary rep to json file'''
+        filename = '{}.json'.format(cls.__name__)
+        ls = []
+        with open(filename, 'w') as f:
+            if list_objs is None:
+                f.write(json.dumps(ls))
+            else:
+                for elem in list_objs:
+                    class_dict = cls.to_dictionary(elem)
+                    ls.append(class_dict)
+                f.write(Base.to_json_string(ls))
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''Turns a string to a python object'''
+        if json_string is None:
+            return []
+        return json.load(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''Create a new object'''
+        if cls.__name__ == 'Rectangle':
+            new_obj = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            new_obj = cls(1)
+        new_obj.update(**dictionary)
+        return new_obj
+
+    @classmethod
+    def load_from_file(cls):
+        '''Gets a alist of object from json file'''
+        filename = '{}.json'.format(cls.__name__)
+        ls = []
+        if os.path.exists(filename):
+            with open(filename, 'r') as f:
+                obj_json_string = f.read()
+                obj_ls = cls.from_json_string(obj_json_string)
+                for elem in obj_ls:
+                    obj = cls.create(**elem)
+                    ls.append(obj)
+        return ls
