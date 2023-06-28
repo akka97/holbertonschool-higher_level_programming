baa82b20fc2c71b4b74feff2968b0a7827ae9b13
akka97 <akapellari@yahoo.com>
2023-06-28 01:32:00 -0700

    tasks

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 '''Inherted class'''
 
+from models.base import base
+
 class Rectangle(Base):
     '''Rectangle class'''
     def __init__(self, width, height, x=0, y=0, id=None):
