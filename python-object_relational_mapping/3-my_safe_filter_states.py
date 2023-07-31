b8d340e9e4baace5f7f71b2a0dd5cf3021a831bc
akka97 <akapellari@yahoo.com>
2023-07-31 06:07:31 -0700

    sql injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""module for getting states that start with letter n"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    u = sys.argv[1]
+    p = sys.argv[2]
+    d = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], charset="utf8")
+    cr = db.cursor()
+    cr.execute("SELECT * FROM states WHERE name LIKE BINARY %s",
+               (sys.argv[4], ))
+    states = cr.fetchall()
+    for state in states:
+        print(state)
+    cr.close()
+    db.close()
