fdea31fda71ed61fc7b9919223221f98adbd684b
akka97 <akapellari@yahoo.com>
2023-07-31 06:15:47 -0700

    citie by states

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""module for getting states that start with letter n"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    u = sys.argv[1]
+    p = sys.argv[2]
+    d = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], charset="utf8")
+    cr = db.cursor()
+    query = " ".join([
+        "SELECT cities.id, cities.name, states.name",
+        "FROM cities JOIN states ON cities.state_id = states.id"
+        ])
+    cr.execute(query)
+    cities = cur.fetchall()
+    for city in cities:
+        print(city)
+    cr.close()
+    db.close()
