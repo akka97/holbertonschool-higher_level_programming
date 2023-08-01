44b19eafc12be69d7696f8baee046a067d81bbc9
akka97 <akapellari@yahoo.com>
2023-08-01 00:58:13 -0700

    all cities

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
                          charset='utf8')
     cur = db.cursor()
     query = " ".join([
-        "SELECT cities.id, cities.name, states.name",
+        "SELECT cities.name",
         "FROM cities JOIN states ON cities.state_id = states.id",
         "WHERE states.name LIKE %s"
         ])
