efe58f92356d0ad9cd8a70abbe28eb94f39a5887
akka97 <akapellari@yahoo.com>
2023-09-14 14:09:36 +0200

    task

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,6 @@
+const updateHeaderButton = document.getElementById('update_header')
+const header = document.querySelector('header');
+
+updateHeaderButton.addEventListener('click', function() {
+    header.textContent = 'New Header!!!';
+})
